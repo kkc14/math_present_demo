069a4e104c218e9a1c7af5dc8d2fcb591529594c
kkc14 <kk141mail@gmail.com>
2024-04-22 23:47:47 +0800

    debug

--- javascripts/Cloudinary.js
+++ javascripts/Cloudinary.js
@@ -12,7 +12,8 @@ const myWidget = cloudinary.createUploadWidget(
     if (!error && result && result.event === "success") {
       //console.log("Done! Here is the image info: ", result.info);
       //document.getElementById("uploadedimage").setAttribute("src", result.info.secure_url);
-      insertAtCursor(textarea, '![My Image]('+ result.info.secure_url +')');
+
+      textarea.value = textarea.value + '![My Image]('+ result.info.secure_url +')' ;
     }
   }
 );
@@ -25,13 +26,4 @@ document.getElementById("upload_widget").addEventListener(
   false
 );
 
-
-function insertAtCursor(textarea, text) {
-  var startPos = textarea.selectionStart;
-  var endPos = textarea.selectionEnd;
-  textarea.value = textarea.value.substring(0, startPos) + text + textarea.value.substring(endPos, textarea.value.length);
-  // Position the cursor after the inserted text
-  textarea.selectionStart = startPos + text.length;
-  textarea.selectionEnd = startPos + text.length;
-}
 
