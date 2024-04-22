9257ace22ec008e65e6f9d7d79ce2962db8a1c4b
kkc14 <kk141mail@gmail.com>
2024-04-23 00:03:31 +0800

    debug

--- javascripts/Cloudinary.js
+++ javascripts/Cloudinary.js
@@ -3,6 +3,13 @@ const uploadPreset = "MathPresenter2324"; // replace with your own upload preset
 var textarea = document.getElementById('input');
 
 
+// Remove the comments from the code below to add
+// additional functionality.
+// Note that these are only a few examples, to see
+// the full list of possible parameters that you
+// can add see:
+//   https://cloudinary.com/documentation/upload_widget_reference
+
 const myWidget = cloudinary.createUploadWidget(
   {
     cloudName: cloudName,
@@ -38,5 +45,4 @@ document.getElementById("upload_widget").addEventListener(
   },
   false
 );
-
 
